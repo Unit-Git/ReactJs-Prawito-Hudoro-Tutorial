e7fd2deb16ae3c1259532e550bc3aa2dfbe04dfa
MuhammadAkbar11 <muhammadakbarletlet@gmail.com>
2020-03-04 21:53:25 +0700

    Membuat Project React dengan create react app

--- src/index.js
+++ src/index.js
@@ -4,7 +4,10 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const HelloWorld = () => {
+	return <p>Hello World</p>;
+};
+ReactDOM.render(<HelloWorld />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
