e1295316e7830d63faee7828a1a87b923f37a9e4
MuhammadAkbar11 <muhammadakbarletlet@gmail.com>
2020-06-12 13:12:48 +0700

    post data ke fake API

--- src/container/BlogPost/BlogPost.jsx
+++ src/container/BlogPost/BlogPost.jsx
@@ -6,10 +6,16 @@ import Axios from 'axios';
 export default class BlogPost extends Component {
 	state = {
 		post: [],
+		formBlogPost: {
+			id: '',
+			title: '',
+			body: '',
+			userId: 1,
+		},
 	};
 
 	handleLoadData = () => {
-		Axios.get('http://localhost:5000/posts')
+		Axios.get('http://localhost:5000/posts?_sort=id&_order=desc')
 			.then((res) => {
 				this.setState({
 					post: res.data,
@@ -18,11 +24,37 @@ export default class BlogPost extends Component {
 			.catch((err) => console.log(err));
 	};
 
+	postDataToApi = (title, body) => {
+		Axios.post('http://localhost:5000/posts', this.state.formBlogPost).then(
+			(res) => {
+				title.value = '';
+				body.value = '';
+				this.handleLoadData();
+				console.log(res);
+			}
+		);
+	};
+
 	handleRemove = (id) => {
 		Axios.delete('http://localhost:5000/posts/' + id).then((res) => {
-			alert('Data Berhasil diHapus!');
 			this.handleLoadData();
 		});
+	};
+
+	handleFormChange = (event) => {
+		let formBlogPostNew = { ...this.state.formBlogPost };
+		let timeStamp = new Date().getTime();
+		formBlogPostNew['id'] = timeStamp;
+		formBlogPostNew[event.target.name] = event.target.value;
+		this.setState({
+			formBlogPost: formBlogPostNew,
+		});
+	};
+
+	handleSubmit = () => {
+		const title = document.querySelector('#title');
+		const body = document.querySelector('#body');
+		this.postDataToApi(title, body);
 	};
 
 	componentDidMount() {
@@ -33,11 +65,50 @@ export default class BlogPost extends Component {
 		return (
 			<Fragment>
 				<h5 className='title-section'>History Post</h5>
-				{this.state.post.map((post) => {
-					return (
-						<CardPost key={post.id} data={post} onDelete={this.handleRemove} />
-					);
-				})}
+				<div className='main--content'>
+					<div className='col--data'>
+						{this.state.post.map((post) => {
+							return (
+								<CardPost
+									key={post.id}
+									data={post}
+									onDelete={this.handleRemove}
+								/>
+							);
+						})}
+					</div>
+					<div className='form--post'>
+						<div className='form--group'>
+							<label htmlFor='title'>Title</label>
+							<br />
+							<input
+								className='form--input'
+								type='text'
+								name='title'
+								id='title'
+								placeholder='Title'
+								onChange={this.handleFormChange}
+							/>
+						</div>
+						<br />
+						<br />
+						<div className='form--group'>
+							<label htmlFor='body'>Contents</label>
+							<br />
+							<textarea
+								className='form--input'
+								name='body'
+								id='body'
+								cols='30'
+								rows='10'
+								onChange={this.handleFormChange}></textarea>
+						</div>
+						<br />
+						<button className='submit' onClick={this.handleSubmit}>
+							Tambah
+						</button>
+					</div>
+				</div>
 			</Fragment>
 		);
 	}
